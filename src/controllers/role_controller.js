2db3abfa9879b1f1a14b6d9a09d99bca33621425
TPMPabasara9 <madushapabasara259@gmail.com>
2026-09-04 11:48:10 +0530

    role based endpoints

--- src/controllers/role_controller.js
+++ src/controllers/role_controller.js
@@ -18,8 +18,71 @@ const createRole = async (req, res) => {
     }
 }
 
-const getAllRoles = async (req, res) => {
+
+const getAllRoles = async (req,res) => {
     try{
-        const roles =  await 
+        const roles = await roleService.getAllRoles();
+        res.status(200).json({
+            message: 'Roles fetched successfully',
+            data: roles
+        });
+
+    }
+    catch(error){
+        res.status(400).json({
+            message: error.message
+        });
     }
 }
+
+const getRoleById = async (req,res) => {
+    try{
+        const roleId = req.params.id;
+        const role = await roleService.getRoleById(roleId);
+        res.status(200).json({
+            message: 'Role fetched successfully',
+            data: role
+        });
+    }catch(error){
+        res.status(400).json({
+            message: error.message
+        });
+    }
+}
+
+const updateRole = async (req,res) => {
+    try{
+        const roleId = req.params.id;
+        const roleData = req.body;
+        const updatedRole = await roleService.updateRole(roleId, roleData);
+        res.status(200).json({
+            message: 'Role updated successfully',
+            data: updatedRole
+        });
+
+    }
+    catch(error){
+        res.status(400).json({
+            message: error.message
+        });
+    }
+}
+
+const deleteRole = async (req,res) => {
+    try{
+        const roleId = req.params.id;
+        const deletedRole = await roleService.deleteRole(roleId);
+        res.status(200).json({
+            message: 'Role deleted successfully',
+            data: deletedRole
+        });
+    }
+    catch(error){
+        res.status(400).json({
+            message: error.message
+        });
+    }
+}
+
+
+module.exports = { createRole, getAllRoles, getRoleById, updateRole, deleteRole };
--- src/service/role_service.js
+++ src/service/role_service.js
@@ -1,29 +1,59 @@
-const bcrypt = require('bycryptjs');
-
 const User = require('../models/user_model.js');
 const Role = require('../models/role_model.js');
 
 
-const createUser = async (userData) => {
-    const { username, email, password, roles } = userData;
+const createRole = async (roleData) => {
+   const existingRole  = await Role.findOne({ name: roleData.name });
+   if(existingRole){
+    throw new Error('Role already exists');
+   }
 
-    const existingUser = await User.findOne({email});
-    if(existingUser){
-        throw new Error('User with this email already exists');
+   return await Role.create(roleData);
+}
+
+const getAllRoles = async () => {
+    return await Role.find();
+}
+
+const getRoleById  = async (id) => {
+const role =  await Role.findById(id);
+if(!role){
+    throw new Error('Role not found');
+}
+
+return role;
+}
+const getRoleByName = async (name) => {
+
+    const role = await Role.findOne({ name: name });
+    if(!role){
+        console.log(`Role with name ${name} not found`);
+        return null;
     }
+    return role;
+}
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+const updateRole = async (id, data) =>{
+    const role = await Role.findByIdAndUpdate(id, data,
+         { new: true 
+            // runValidators: true
+         }
+        );
+    if(!role){
+        throw new Error('Role not found');
+    }
+    return role;
+}
 
-    const user = new User({
-        username,
-        email,
-        password: hashedPassword,
-        roles
-    });
+const deleteRole = async (id) =>{
+    const role = await Role.findByIdAndDelete(id);
+    if(!role){
+        throw new Error('Role not found');
+    }
+    return role;
+}
 
-    return await user.save();
-};
-
+module.exports = {createRole, getAllRoles,getRoleById, updateRole, deleteRole,getRoleByName};
 
 
 
