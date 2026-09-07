91e5b189032a6574fda1758ceff1cbc4cda8b235
TPMPabasara9 <madushapabasara259@gmail.com>
2026-09-07 16:11:45 +0530

    added the authentication and authorization middlewares to the user routes

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user_controller');
+const authenticationMiddleware = require('../middleware/authentication');
+const authorizationMiddleware = require('../middleware/authorization');
 
-router.post('/createUser', userController.createUser);
-router.get('/getUsers', userController.getAllUsers);
-router.get("/getUsers/:id", userController.getUserById);
+
+
+router.post('/createUser', authenticationMiddleware.authenticate,
+     authorizationMiddleware.authorize(['admin']),
+      userController.createUser);
+
+router.get('/getUsers', authenticationMiddleware.authenticate,
+     authorizationMiddleware.authorize(['admin']),
+    userController.getAllUsers);
+
+router.get('/getUser', authenticationMiddleware.authenticate,
+     authorizationMiddleware.authorize(['admin']),
+    userController.getUserById);
+
+router.put('/updateUser/:id', authenticationMiddleware.authenticate,
+      authorizationMiddleware.authorize(['admin']),
+     userController.updateUser);
+
+router.delete('/deleteUser/:id', authenticationMiddleware.authenticate,
+      authorizationMiddleware.authorize(['admin']),
+     userController.deleteUser);
+
 
 module.exports = router;
