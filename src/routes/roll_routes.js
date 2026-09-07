e0ae264733cab5ab2a090165840fb9c141deb3f4
TPMPabasara9 <madushapabasara259@gmail.com>
2026-09-07 16:11:18 +0530

    add the middleware to roles crud endpoints routes

--- src/routes/roll_routes.js
+++ src/routes/roll_routes.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const roleController = require("../controllers/role_controller");
+const authenticationMiddleware = require("../middleware/authentication");
+const authorizationMiddleware = require("../middleware/authorization");
 
-router.post("/createRoles", roleController.createRole);
-router.get("/getRoles", roleController.getAllRoles);
-router.get("/getRoles/:id", roleController.getRoleById);
-router.put("/updateRoles/:id", roleController.updateRole);
-router.delete("/deleteRoles/:id", roleController.deleteRole);
+router.post("/createRoles",
+    authenticationMiddleware.authenticate,
+    authorizationMiddleware.authorize(['admin']),
+    roleController.createRole);
+
+router.get("/getRoles",
+    authenticationMiddleware.authenticate,
+    authorizationMiddleware.authorize(['admin']),
+    roleController.getAllRoles);
+
+
+router.get("/getRoles/:id",
+    authenticationMiddleware.authenticate,
+    authorizationMiddleware.authorize(['admin']),
+    roleController.getRoleById);
+
+router.put("/updateRoles/:id",
+    authenticationMiddleware.authenticate,
+    authorizationMiddleware.authorize(['admin']),
+    roleController.updateRole);
+
+router.delete("/deleteRoles/:id",
+    authenticationMiddleware.authenticate,
+    authorizationMiddleware.authorize(['admin']),
+    roleController.deleteRole);
 
 module.exports = router;
