f3ba16ca3c50cdcf7599d458f4c074ceff402849
TPMPabasara9 <madushapabasara259@gmail.com>
2026-09-07 16:08:11 +0530

    add the user routes to the app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const rolesRoute = require("./routes/roll_routes");
 const userRoute = require("./routes/user_routes");
+const authRoute = require("./routes/auth_routes");
 console.log("Express loaded");
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/api/roles", rolesRoute);
 app.use("/api/users", userRoute);
+app.use("/api/auth",authRoute );
 
 app.get("/",(req,res) =>{
     res.send("Welcome to the Role Management API");
