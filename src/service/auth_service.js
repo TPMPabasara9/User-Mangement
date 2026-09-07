bd0906760233a91e609fc1adb0aedb2e7a0a8a27
TPMPabasara9 <madushapabasara259@gmail.com>
2026-09-07 16:09:56 +0530

    seperate the datbse logic from the controller for login

---
+++ src/service/auth_service.js
@@ -0,0 +1,25 @@
+const User = require('../models/user_model.js');
+const roleService = require('./role_service.js');
+const bcrypt = require('bcryptjs');
+const JWT = require("jsonwebtoken");
+
+
+const login = async (email, password) => {
+    const user = await User.findOne({ email }).populate('roles', 'name');
+    if (!user) return null;
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return null;
+
+    const token = JWT.sign({
+        id: user._id,
+        email: user.email,
+        roles: user.roles.map(role => role.name)
+    }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    
+
+    return { user, token };
+    
+}
+
+module.exports = { login };
