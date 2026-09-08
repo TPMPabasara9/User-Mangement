22811a6737c1ff589608f3a84ac7f753b8ee614b
TPMPabasara9 <madushapabasara259@gmail.com>
2026-09-08 06:48:04 +0530

    added validation middleware to the user routes

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 const authenticationMiddleware = require('../middleware/authentication');
 const authorizationMiddleware = require('../middleware/authorization');
+const { validationMiddleware } = require('../middleware/validation');
+const { createUserSchema, updateUserSchema } = require('../validation/userValidation');
 
 
 
 router.post('/createUser', authenticationMiddleware.authenticate,
      authorizationMiddleware.authorize(['admin']),
+     validationMiddleware(createUserSchema),
       userController.createUser);
 
 router.get('/getUsers', authenticationMiddleware.authenticate,
@@ -20,6 +23,7 @@ router.get('/getUser', authenticationMiddleware.authenticate,
 
 router.put('/updateUser/:id', authenticationMiddleware.authenticate,
       authorizationMiddleware.authorize(['admin']),
+     validationMiddleware(updateUserSchema),
      userController.updateUser);
 
 router.delete('/deleteUser/:id', authenticationMiddleware.authenticate,
